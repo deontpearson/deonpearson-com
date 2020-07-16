a770ac8c905a42b3ec9d6e467b0cbd6eaacc0588
Deon Pearson <deon.pearson@takealot.com>
2020-07-17 00:04:41 +0200

    testing actions v2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Contact() {
   return (
     <div className="Contact">
-      <h1>hello@deonpearson.com</h1>
+      <h1>Email: hello@deonpearson.com</h1>
     </div>
   );
 }
