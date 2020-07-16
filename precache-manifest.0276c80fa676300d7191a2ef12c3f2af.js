8ee8bfd1853bcff8740fd3cc71f352b666637427
Deon Pearson <deon.pearson@takealot.com>
2020-07-16 23:13:46 +0200

    Updates

---
+++ precache-manifest.0276c80fa676300d7191a2ef12c3f2af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005c682323a1fef1594ec74d37ea33a0",
+    "url": "/deonpearson-com/index.html"
+  },
+  {
+    "revision": "ea828f6fe6d4d8de99e9",
+    "url": "/deonpearson-com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5421884d39d236d04823",
+    "url": "/deonpearson-com/static/js/2.596b8375.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deonpearson-com/static/js/2.596b8375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea828f6fe6d4d8de99e9",
+    "url": "/deonpearson-com/static/js/main.423f56c5.chunk.js"
+  },
+  {
+    "revision": "faf9365e0ea98dfc4936",
+    "url": "/deonpearson-com/static/js/runtime-main.57870e00.js"
+  }
+]);
